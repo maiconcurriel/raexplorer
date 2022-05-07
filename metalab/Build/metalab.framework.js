3a91afd3be81eb5495ba29fb83dbc18365d461b2
Maicon Douglas Curriel <maiconcurriel@gmail.com>
2022-05-07 20:07:21 -0300

    metatest6

--- metalab/Build/metalab.framework.js
+++ metalab/Build/metalab.framework.js
@@ -1286,7 +1286,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2928880;
+STATICTOP = STATIC_BASE + 2929408;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3312,7 +3312,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2928880;
+var STATIC_BUMP = 2929408;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
