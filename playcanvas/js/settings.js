b27dbcf0df1f802432bb89b90e211ed8bb52d981
maiconcurriel <72510390+maiconcurriel@users.noreply.github.com>
2024-03-14 14:24:37 -0300

    update HTML3D

--- playcanvas/js/settings.js
+++ playcanvas/js/settings.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "1966207.json";
+window.SCENE_PATH = "js/1966207.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -10,7 +10,7 @@ window.CONTEXT_OPTIONS = {
     'powerPreference': "default"
 };
 window.SCRIPTS = [ 170903679, 170903986, 170922609, 170922807, 170922808, 170922809 ];
-window.CONFIG_FILENAME = "config.json";
+window.CONFIG_FILENAME = "js/config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
