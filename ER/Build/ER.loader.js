620283146ccc8135db9bfa622fe312e70d0ab6c7
maiconcurriel <maiconcurriel@gmail.com>
2025-02-17 10:20:05 -0300

    170225a

--- ER/Build/ER.loader.js
+++ ER/Build/ER.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97626168,
+    wasmFileSize: 97626160,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
