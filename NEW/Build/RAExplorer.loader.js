cb3f21f35fbde3526cb583b6c47640c1c64ea0a2
maiconcurriel <maiconcurriel@gmail.com>
2025-02-17 18:45:31 -0300

    170225raI

--- NEW/Build/RAExplorer.loader.js
+++ NEW/Build/RAExplorer.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 74718046,
+    wasmFileSize: 74498301,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
